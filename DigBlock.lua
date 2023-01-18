155b242cac9b72127fae91dd8ee1be74434e6512
VenerableTimeWanderer <57559691+VenerableTimeWanderer@users.noreply.github.com>
2023-01-18 18:16:40 +1100

    Add files via upload
    Slightly less basic

--- DigBlock.lua
+++ DigBlock.lua
@@ -1,19 +1,37 @@
 local ac = peripheral.wrap("right")
-local blockPlaced = false
+local blockPlaced = false --Test variable for whether or not a block is placed
+local pick = turtle.getItemDetail(1, true)
 
-while true do
+local dur = pick.maxDamage - pick.damage
+
+while true do --Wont stop till broken
     
-    while turtle.getItemCount(2) == 0 do
-        turtle.suckUp()
+	while dur >= 1 do --If pick durability is equal to 1 stop
+	
+		while turtle.getItemCount(16) <= 0 do
+		
+			while turtle.getItemCount(2) == 0 do --Makes sure item is stacked
+				turtle.suckUp() --Wont stop even if chest is empty
+			end
+			
+			turtle.select(2)
+			turtle.place()
+			turtle.select(1)
+			ac.digBlock()
+			ac.collectItems()
+			sleep(1)
+		end
+		for i=1, 14 do --Empties all slots bar 1 and 2
+			turtle.select(i+2)
+			turtle.dropDown()
+		end
+		pick = turtle.getItemDetail(1, true)
+		dur = pick.maxDamage - pick.damage
+		
+		print(("Pick durability: %d/%d"):format(dur, pick.maxDamage))
     end
-    
-    turtle.select(2)
-    turtle.place()
-    turtle.select(1)
-    ac.digBlock()
-    ac.collectItems()
-    sleep(1)
-    
+	print("Change pick")
+	sleep(15)
 end
     
     
