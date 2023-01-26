d5fabfc12b940e17dbaa44c584f47885e4a62e26
VenerableTimeWanderer <57559691+VenerableTimeWanderer@users.noreply.github.com>
2023-01-26 14:22:44 +1100

    Add files via upload
    Filter manager

--- invManager.lua
+++ invManager.lua
@@ -1,3 +1,9 @@
+local gate = false
+local match = false
+local myInventory
+local chatEvent
+local tagFilter = {"forge:ores", "forge:raw_materials"}
+
 local manager = peripheral.find("inventoryManager")
 local box = peripheral.find("chatBox")
 
@@ -6,17 +12,46 @@ if box == nill then error("chatBox not found") end
 
 print("Emptying...")
 
-while true do 
-    event, username, message = os.pullEvent("chat")
-
-    if username == manager.getOwner() and message == "dump" then
-        print("Dumping slot 9 to 17")
-        myInventory = manager.getItems()
-        for keys, vector in pairs(myInventory) do
-            if vector.slot > 8 and vector.slot < 18 then
-                print(("Slot:%d Name:%s Count:%d"):format(vector.slot, vector.name, vector.count))
-                manager.removeItemFromPlayer("LEFT", vector.count, vector.slot)
-            end
+local function chatReader()
+    while true do 
+        chatEvent = {os.pullEvent("chat")}
+        
+        if chatEvent[3] == "dump" and chatEvent[2] == manager.getOwner() then
+            box.sendMessageToPlayer("Dump System Online", chatEvent[2])
+            gate = true
+         elseif chatEvent[3] == "stop" and chatEvent[2] == manager.getOwner() then
+            box.sendMessageToPlayer("Dump System Offline", chatEvent[2])
+            gate = false
         end
     end
 end
+
+local function invDump()
+    while true do
+        if gate then
+            myInventory = manager.getItems() 
+            for keysItem, item in pairs(myInventory) do
+                if item ~= nil then 
+                    if item.tags ~= nil then
+                        for keysTags, itemTag in pairs(item.tags) do
+                            for keysFilter, filterTag in pairs(tagFilter) do
+                                if string.find(itemTag, filterTag) then
+                                    match = true
+                                    break
+                                end
+                            end
+                        end
+                    end
+                end
+                if match then
+                    box.sendMessageToPlayer("Items Dumped", chatEvent[2])
+                    manager.removeItemFromPlayer("TOP", item.count, item.slot)
+                    match = false
+                end
+            end
+        end
+        os.sleep(5)
+    end
+end
+
+parallel.waitForAny(chatReader, invDump)
